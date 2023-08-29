176e806f31dd2ff4f86fa2079070ec316430c334
Siphephelo Myeni <sipheanele@gmail.com>
2023-08-29 17:29:28 +0200

    Server Service

--- src/app/interface/custom-response.ts
+++ src/app/interface/custom-response.ts
@@ -7,5 +7,5 @@ export interface CustomResponse{
   reason: string;
   message: string;
   developerMessage: string;
-  data: { servers?: Server[], server?: Server}
+  data: { servers?: Server[], server?: Server};
 }
